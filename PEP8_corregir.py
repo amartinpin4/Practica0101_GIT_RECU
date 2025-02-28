514fb0202abba72059059c8894791d38ad5d1e18
amartinpin4 <amartinpin4@educacion.navarra.es>
2025-02-28 19:35:54 +0100

    Update PEP8_corregir.py

--- PEP8_corregir.py
+++ PEP8_corregir.py
@@ -1,12 +1,17 @@
 # script.py
-import os, sys
+import os
+import sys
+
+
 def MiFuncionSuma(A, B, C, imprime = True):
-    resultado=A+B+C
+    resultado = A + B + C
     if imprime != False:
         print(resultado)
     return resultado
-a          = 4
+
+
+a = 4
 variable_b = 5
-var_c      = 10
+var_c = 10
 
 MiFuncionSuma(a, variable_b, var_c)
